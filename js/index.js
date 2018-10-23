dba8647b27c8b1fabc59d3d0c4f0734acd8a3aee
Narongrit <s5830213040@phuket.psu.ac.th>
2018-10-23 23:33:03 +0700

    Status posotion

--- js/index.js
+++ js/index.js
@@ -20,3 +20,91 @@ document.addEventListener('prechange', function(event) {
   });
 
 
+
+  function checkIdUser(uid) {
+    var mainId;
+    $.ajax({
+
+        //CP1. Complete Ajax Code to GET ALL pin data 
+        type: "GET",
+        url: "https://reststepvx20180923124000.azurewebsites.net/Api/Staffs",
+
+    }).then(function (data) {
+
+        // document.getElementById("firstQueue").innerHTML = data.Name;
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].Uid === uid) {
+                mainId = data[i].Id;
+            }
+        }
+
+        console.log(mainId);
+
+        checkPosition(mainId);
+        checkUid(mainId);
+
+    });
+}
+function checkUid(mainId) {
+
+    $.ajax({
+
+        //CP1. Complete Ajax Code to GET ALL pin data 
+        type: "GET",
+        url: "https://reststepvx20180923124000.azurewebsites.net/Api/Staffs/" + mainId,
+
+    }).then(function (data) {
+
+        document.getElementById("showFname").innerHTML = data.FirstName;
+
+    });
+}
+
+function checkPosition(mainId) {
+    $.ajax({
+
+        //CP1. Complete Ajax Code to GET ALL pin data 
+        type: "GET",
+        url: "https://reststepvx20180923124000.azurewebsites.net/Api/Staffs/" + mainId,
+
+    }).then(function (data) {
+
+        // document.getElementById("firstQueue").innerHTML = data.Name;
+
+        if (data.Position === "Admin") {
+            console.log("I'm Admin");
+        } else if (data.Position === "Queue") {
+            console.log("I'm Queue");
+        } else if (data.Position === "Table") {
+            console.log("I'm Table");
+        } else if (data.Position == "Chef") {
+            console.log("I'm Chef");
+        } else {
+            console.log("Don't have position");
+        }
+
+
+
+    });
+
+}
+
+function autoLogIn(email,password) {
+
+ 
+  // var credential = firebase.auth.EmailAuthProvider.credential(email, password);
+  firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(function (error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          if (errorCode === 'auth/wrong-password') {
+              alert('Wrong password.');
+          } else {
+              alert(errorMessage);
+          }
+          console.log(error);
+      });
+
+
+}
